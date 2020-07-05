874196a690abddc80b89128a7bf56fd5f70bcd79
Viacheslav Pleshkov <mail@viacheslavpleshkov.com>
2020-07-05 18:41:41 +0300

    Update files

--- client/src/main.c
+++ client/src/main.c
@@ -1,77 +1,74 @@
 #include "uchat_client.h"
 
 void *input(void *sock) {
-    int *sockfd = (int *) sock;
-    char recvBuff[1024];
-    char name[100];
-    char *json_str = NULL;
-    t_json_data *json = malloc(sizeof(t_json_data));
-    enum e_json_types type;
+int *sockfd = (int *) sock;
+char *json_str = NULL;
+t_json_data *json = malloc(sizeof(t_json_data));
+enum e_json_types type;
 
-    type = JS_MES_OUT;
-    json->message.client1_id = 1;
-    json->message.client2_id = 2;
-    json->message.text = mx_strdup("hallo");
+type = JS_REG;
+strcpy(json->pers_info.login, "spleshkov");
+strcpy(json->pers_info.password, "qwerty");
+strcpy(json->pers_info.first_name, "Viacheslav");
+strcpy(json->pers_info.last_name, "Pleshkov");
 //        while(1) {
-    json_str = mx_json_make_json(type, json);
-    mx_printint(mx_strlen(json_str));
-    write(*sockfd, json_str, mx_strlen(json_str));
-    memset(recvBuff, '\0', 1024);
-    memset(name, '\0', 100);
+json_str = mx_json_make_json(type, json);
+mx_printint(mx_strlen(json_str));
+write(*sockfd, json_str, mx_strlen(json_str));
 //    }
 
-    return NULL;
+return NULL;
 }
 
 int main(int argc, char **argv) {
-    int sockfd = 0;
-    struct sockaddr_in serv;
-    char buf[1024];
-    pthread_t thread = NULL;
-    char name[100];
+int sockfd = 0;
+struct sockaddr_in serv;
+pthread_t thread = NULL;
+char name[100];
 //    t_json_data *json = NULL;
 
-    if (argc != 3) {
-        mx_printerr("uchat_server: error args\n");
-        mx_printerr("example: ./uchat ip port\n");
-        exit(1);
-    }
-
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        mx_printerr("uchat: couldn't create socket");
-        exit(1);
-    }
-
-    serv.sin_family = AF_INET;
-    serv.sin_port = htons(atoi(argv[2]));
-
-    if ((inet_pton(AF_INET, argv[1], &serv.sin_addr)) <= 0) {
-        mx_printerr("uchat: network adress isn't valid");
-        exit(1);
-    }
-    if ((connect(sockfd, (struct sockaddr *)&serv, sizeof(serv))) < 0) {
-        mx_printerr("uchat: connection failed\n");
-        exit(1);
-    }
-    memset(name, '\0', 100);
-    mx_printstr("YOUR NAME: ");
-    scanf("%s", name);
-    write(sockfd, name, mx_strlen(name));
-    if (pthread_create(&thread, NULL, input, &sockfd) != 0) {
-        mx_printerr("uchat_server: thread creating error");
-        exit(1);
-    }
-    while (1) {
-        recv(sockfd, buf, 1024, 0);
-//        json = mx_json_parse(buf);
-
-        mx_printstr("Message recived: ");
-        mx_printstr(buf);
-//        mx_printstr(json->data.message.text);
-        mx_printstr("\n from: ");
-//        mx_printint(json->data.message.client1_id);
-        memset(buf, '\0', 1024);
-//        mx_strdel(&json->data.message.text);
-//        free(json);
-    }
+if (argc != 3) {
+mx_printerr("uchat_server: error args\n");
+mx_printerr("example: ./uchat ip port\n");
+exit(1);
 }
+
+if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+mx_printerr("uchat: couldn't create socket");
+exit(1);
+}
+
+serv.sin_family = AF_INET;
+serv.sin_port = htons(atoi(argv[2]));
+
+if ((inet_pton(AF_INET, argv[1], &serv.sin_addr)) <= 0) {
+mx_printerr("uchat: network adress isn't valid");
+exit(1);
+}
+if ((connect(sockfd, (struct sockaddr *)&serv, sizeof(serv))) < 0) {
+mx_printerr("uchat: connection failed\n");
+exit(1);
+}
+//    memset(name, '\0', 100);
+//    mx_printstr("YOUR NAME: ");
+//    scanf("%s", name);
+write(sockfd, name, mx_strlen(name));
+if (pthread_create(&thread, NULL, input, &sockfd) != 0) {
+mx_printerr("uchat_server: thread creating error");
+exit(1);
+}
+//    while (1) {
+//        recv(sockfd, buf, 1024, 0);
+////        json = mx_json_parse(buf);
+//
+//        mx_printstr("Message recived: ");
+//        mx_printstr(buf);
+////        mx_printstr(json->data.message.text);
+//        mx_printstr("\n from: ");
+////        mx_printint(json->data.message.client1_id);
+//        memset(buf, '\0', 1024);
+////        mx_strdel(&json->data.message.text);
+////        free(json);
+//    }
+}
+
--- server/src/mx_routes.c
+++ server/src/mx_routes.c
@@ -1,9 +1,9 @@
 #include "uchat_server.h"
 
 void mx_routes(t_json_data *json, t_clients *client, t_clients *cur_client) {
-    if (json->type == JS_LOG_OUT)
+    if (json->type == JS_REG)
         mx_controller_register();
-    if(json->type == JS_LOG_OUT)
+    if(json->type == JS_LOG_IN)
         mx_controller_login();
     if (json->type == JS_LOG_OUT)
         mx_controller_log_out();
