6446e11fea687c3294d9d6f3d32b2632776f596b
arni30 <alex30jordan@gmail.com>
2020-07-10 18:37:34 +0300

    fixed messages

--- server/src/mx_routes.c
+++ server/src/mx_routes.c
@@ -8,5 +8,5 @@ void mx_routes(t_json_data *json, t_clients *client, t_clients *cur_client) {
     if (json->type == JS_LOG_OUT)
         mx_controller_log_out();
     if (json->type == JS_MES_OUT)
-        mx_controller_message(client, cur_client, json);
+        mx_controller_message(client, json);
 }
